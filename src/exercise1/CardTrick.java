b2a2f9a5ddc19e7b15468979aa321a473ee02117
shmaybe <122491122+shmaybe@users.noreply.github.com>
2023-02-03 12:58:23 -0500

    added info

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -66,26 +66,26 @@ public class CardTrick {
     /**
      * A simple method to print out personal information. Follow the instructions to 
      * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
+     * @author May Tran Feb 3, 2023
      */
     private static void printInfo() {
     
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is May");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- stop crying");
+        System.out.println("-- have a farm");
 	System.out.println();	
 
         System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("-- crying");
+        System.out.println("-- stardew");
+        System.out.println("-- nuggets");
+        System.out.println("-- more crying");
 
         System.out.println();
         
